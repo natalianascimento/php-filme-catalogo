70bebd991fdeb5458b2c9d50caab16f5ab734844
Natalia <natalia.nascc@gmail.com>
2026-06-08 16:10:42 -0300

    Reorganizando projeto

--- screen-match/src/Modelo/Filme.php
+++ screen-match/src/Modelo/Filme.php
@@ -1,8 +1,42 @@
 <?php
 
 class Filme {
-    public string $nome;
-    public int $anoLancamento;
-    public string $genero;
-    public float $nota;
+
+    private array $notas;
+
+    public function __construct(
+        private string $nome,
+        private int $anoLancamento,
+        private string $genero
+    ) {
+        $this->notas = [];
+    }
+
+    public function avalia(float $nota): void 
+    {
+        $this->notas[] = $nota;   
+    }
+
+    public function media(): float
+    {
+        $somaNotas = array_sum($this->notas);
+        $quantidadeNotas = count($this->notas);
+
+        return $somaNotas / $quantidadeNotas;
+    }
+
+    public function anoLancamento(): int
+    {
+        return $this->anoLancamento;
+    }
+
+    public function nome(): string
+    {
+        return $this->nome;
+    }
+
+    public function genero(): string
+    {
+        return $this->genero;
+    }
 }
