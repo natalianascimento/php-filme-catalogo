344b2470a89c2b72ca12c09d213bd2a99914c6a1
Natalia <natalia.nascc@gmail.com>
2026-06-18 22:57:45 -0300

    Exception

--- screen-match/erro.php
+++ screen-match/erro.php
@@ -4,10 +4,16 @@ require 'autoload.php';
 
 use ScreenMatch\Modelo\{Genero, Serie, Episodio};
 use ScreenMatch\Calculos\ConversorNotaEstrela;
+use ScreenMatch\Exception\NotaInvalidaException;
 
-$serie = new Serie('Nome da Serie', 2024, Genero::Acao, 7, 20, 30);
-$episodio = new Episodio($serie, 'Piloto', 1);
-// $episodio->avalia(10);
+try {
+    $serie = new Serie('Nome da Serie', 2024, Genero::Acao, 7, 20, 30);
+    $episodio = new Episodio($serie, 'Piloto', 1);
+    $episodio->avalia(45);
+    $episodio->avalia(-35);
 
-$conversor = new ConversorNotaEstrela();
-echo $conversor->converte($episodio) . "\n"; 
+    $conversor = new ConversorNotaEstrela();
+    echo $conversor->converte($episodio) . "\n"; 
+} catch (NotaInvalidaException $e) {
+    echo "Um problema aconteceu: " . $e->getMessage() . "\n";
+}
--- screen-match/src/Modelo/ComAvaliacao.php
+++ screen-match/src/Modelo/ComAvaliacao.php
@@ -2,12 +2,20 @@
 
 namespace ScreenMatch\Modelo;
 
+use ScreenMatch\Exception\NotaInvalidaException;
+
 trait ComAvaliacao
 {
     private array $notas = [];
 
+    /**
+     * @throws NotaInvalidaException Se a nota for negativa ou maior que 10.
+     */
     public function avalia (float $nota): void
     {
+        if ($nota < 0 || $nota > 10) {
+            throw new NotaInvalidaException('Nota deve ser entre 0 e 10.');
+        }
         $this->notas[] = $nota;
     }
 
